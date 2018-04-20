e116422be71e9f0e730648a3e1f52bebfe974c6c
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2018-04-20 15:18:16 -0400

    check file extension

--- voxel_similarity.py
+++ voxel_similarity.py
@@ -1,6 +1,9 @@
+from sys import argv
+from sys import exit
 import math
 
 r = 10
+
 
 def create_empty_voxel():
     z = lambda: {i : False for i in range(0, r)}
@@ -27,19 +30,44 @@ def load_from_goxel(filename):
     return v
 
 
-reference = load_from_goxel('./voxel_spaces/goxel/10.txt')
-subject = load_from_goxel('./voxel_spaces/goxel/not10.txt')
-
-max_voxels = math.pow(len(range(0, r)), 3)
-diff = 0
-
-for x, a in subject.items():
-    for y, b in a.items():
-        for z, v in b.items():
-            if v != reference[x][y][z]:
-                diff += 1
+def load_from_voxelizer(filename):
+    return {}
 
 
-percent_similar = 100 - (diff / max_voxels) * 100.0
+def load(filename):
+    if filename[-4:] == '.txt':
+        return load_from_goxel(filename)
+    elif filename[-5:] == '.json':
+        return load_from_voxelizer(filename)
+    else:
+        print('invalid file given: ' + filename)
+        exit(0)
 
-print(percent_similar)
+
+if len(argv) != 3:
+    print('incorrect number of arguments given')
+    exit(0)
+
+else:
+    reference_name = argv[1]
+    subject_name = argv[2]
+
+    if reference_name[-4:] == '.txt':
+        reference = load_from_goxel(reference_name)
+
+    reference = load_from_goxel(reference_name)
+    subject = load_from_goxel(subject_name)
+
+    max_voxels = math.pow(len(range(0, r)), 3)
+    diff = 0
+
+    for x, a in subject.items():
+        for y, b in a.items():
+            for z, v in b.items():
+                if v != reference[x][y][z]:
+                    diff += 1
+
+
+    percent_similar = 100 - (diff / max_voxels) * 100.0
+
+    print(percent_similar)
