4e9bfcc9af754dc49c4f1b2c625e4544b0e00f80
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2018-04-19 23:14:02 -0400

    variable size

--- voxel_similarity.py
+++ voxel_similarity.py
@@ -1,9 +1,11 @@
-#import math
+import math
+
+r = 10
 
 def create_empty_voxel():
-    z = lambda: {i: False for i in range(-10, 10 + 1)}
-    y = lambda: {i:z() for i in range(-10, 10 + 1)}
-    return {i:y() for i in range(-10, 10 + 1)}
+    z = lambda: {i : False for i in range(-r, r + 1)}
+    y = lambda: {i : z() for i in range(-r, r + 1)}
+    return {i : y() for i in range(-r, r + 1)}
 
 
 def load_voxel(filename):
@@ -25,22 +27,10 @@ def load_voxel(filename):
     return v
 
 
-def get_total_voxels(v):
-    count = 0
-
-    for x, a in v.items():
-        for y, b in a.items():
-            for z, c in b.items():
-                if c:
-                    count += 1
-
-    return count
-
-
 reference = load_voxel('1.txt')
-subject = load_voxel('0.75.txt')
+subject = load_voxel('2.txt')
 
-#max_num_voxels = math.pow(len(range(-10, 10 + 1)), 3)
+max_voxels = math.pow(len(range(-r, r + 1)), 3)
 diff = 0
 
 for x, a in subject.items():
@@ -50,8 +40,6 @@ for x, a in subject.items():
                 diff += 1
 
 
-reference_total = get_total_voxels(reference)
-percent_diff = (diff / reference_total) * 100.0
-#percent_diff = (diff / max_num_voxels) * 100.0
+percent_similar = 100 - (diff / max_voxels) * 100.0
 
-print(percent_diff)
+print(percent_similar)
