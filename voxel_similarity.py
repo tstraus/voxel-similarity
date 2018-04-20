e33e70af11497126595bf5da0eb0ae5b7b3a1586
Tucker Strausbaugh <tckrsstrsbgh@gmail.com>
2018-04-19 22:49:41 -0400

    might be working?

--- voxel_similarity.py
+++ voxel_similarity.py
@@ -1,5 +1,4 @@
-from pprint import pprint
-
+#import math
 
 def create_empty_voxel():
     z = lambda: {i: False for i in range(-10, 10 + 1)}
@@ -26,4 +25,33 @@ def load_voxel(filename):
     return v
 
 
-pprint(load_voxel('1.txt'))
+def get_total_voxels(v):
+    count = 0
+
+    for x, a in v.items():
+        for y, b in a.items():
+            for z, c in b.items():
+                if c:
+                    count += 1
+
+    return count
+
+
+reference = load_voxel('1.txt')
+subject = load_voxel('0.75.txt')
+
+#max_num_voxels = math.pow(len(range(-10, 10 + 1)), 3)
+diff = 0
+
+for x, a in subject.items():
+    for y, b in a.items():
+        for z, v in b.items():
+            if v != reference[x][y][z]:
+                diff += 1
+
+
+reference_total = get_total_voxels(reference)
+percent_diff = (diff / reference_total) * 100.0
+#percent_diff = (diff / max_num_voxels) * 100.0
+
+print(percent_diff)
